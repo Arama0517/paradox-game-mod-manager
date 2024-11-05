918c8c6bff7739b982c66f7f97e084574fa9debc
Arama <arama.dev@outlook.com>
2024-11-05 17:16:13 +0800

    fix: 修复初始化项目的问题

--- tools/setup_project.py
+++ tools/setup_project.py
@@ -1,7 +1,7 @@
 import json
+from pathlib import Path
 
-from src.path import LAUNCHER_SETTINGS_FILE_PATH
-
+LAUNCHER_SETTINGS_FILE_PATH = Path.cwd() / 'launcher-settings.json'
 DATA = {
     'gameId': 'hoi4',
     'gameDataPath': '%USER_DOCUMENTS%/Paradox Interactive/Hearts of Iron IV',
